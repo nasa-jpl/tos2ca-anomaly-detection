18eb4234924806dd86c479e1f3f9ab90f3b50470
Brian Knosp <Brian.W.Knosp@jpl.nasa.gov>
2025-10-09 11:00:24 -0700

    Adding new wildfire mask plot

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -7,10 +7,12 @@ import cartopy.mpl.ticker as cticker
 import cartopy.crs as ccrs
 import numpy as np
 import random
-from shapely.geometry import Polygon
 import xarray as xr
 import s3fs
 import boto3
+import shapefile
+import cartopy.feature as cfeature
+import netCDF4 as nc
 
 from copy import deepcopy
 from datetime import datetime as dt
@@ -19,7 +21,10 @@ from database.elasticache import getData
 from utils.s3 import s3Upload
 from database.queries import updateStatus
 from utils import tos2ca_secrets
-
+from glob import glob
+from shapely.geometry import Polygon, shape, MultiPolygon
+from matplotlib.colors import ListedColormap
+from matplotlib.patches import Circle
 
 def get_anno_coords(lat, lon, mask):
     series_ids = np.unique(mask[mask > 0])
@@ -405,4 +410,144 @@ def interpolated_plot_all(jobID, timestamp):
     return
 
 
+def wildfirePlot(maskFile, shapeFile):
+    """
+    This function was used specifically to generate plots of the
+    pre-defined California Wildfire masks.  These were processed
+    after the masks were ggenerated on the local system.  This
+    probably could be used to generate masks for any pre-defined
+    phenomena.  You do also need to have the shapefile you want to use.
+    In this case, we use one for the Unite States only since we
+    were interested in California.  Update in the code as needed, but
+    the shapefiles for the US can be found at:
+    https://catalog.data.gov/dataset/tiger-line-shapefile-2023-nation-u-s-county-and-equivalent-entities
+    :param maskFile: local path and filename of the mask file (*.nc4)
+    :type maskFile: str
+    :param shapeFile: local path and filename of the shapefile (*.shp)
+    :type shapeFile: str
+    """
+    fileList = glob(maskFile)
+    for f in fileList:
+        data = nc.Dataset(f, 'r')
+        timestamp = [i for i in data['masks'].groups.keys()][0]
+        mask_indices = np.asarray(data['masks'][timestamp]['mask_indices'][:])
+        lat = np.asarray(data['navigation']['lat'][:])
+        lon = np.asarray(data['navigation']['lon'][:])
+        empty = np.zeros((len(lat), len(lon)), dtype=int)
+        for i, j, val in mask_indices:
+            empty[i][j] = 1
+        g_lat = []
+        g_lon = []
+        for i, j, val in mask_indices:
+            g_lat.append(lat[i])
+            g_lon.append(lon[j])
+        g_lat = np.asarray(g_lat)
+        g_lon = np.asarray(g_lon)
 
+        cmap = ListedColormap(['white', 'red'])
+
+        # Load the shapefile
+        sf = shapefile.Reader(shapeFile)
+
+        # Set up the map
+        fig, ax = plt.subplots(figsize=(8, 10), subplot_kw={'projection': ccrs.PlateCarree()})
+        ax.set_extent([lon.min(), lon.max(), lat.min(), lat.max()], crs=ccrs.PlateCarree())
+
+        # Add coastlines and state boundaries
+        ax.coastlines(resolution='10m')
+        ax.add_feature(cfeature.STATES, edgecolor='gray')
+
+        # Plot only California counties (FIPS state code 06)
+        for record, geom in zip(sf.records(), sf.shapes()):
+            if record[0] == '06':  # California FIPS code
+                county_geom = shape(geom.__geo_interface__)
+                
+                if isinstance(county_geom, Polygon):
+                    polygons = [county_geom]
+                elif isinstance(county_geom, MultiPolygon):
+                    polygons = county_geom.geoms
+                else:
+                    continue  # Skip if not polygonal
+
+                # Plot each polygon
+                for poly in polygons:
+                    xs, ys = poly.exterior.xy
+                    ax.plot(xs, ys, transform=ccrs.PlateCarree(), color='black', linewidth=0.5)
+
+
+        # Turn off axis
+        name = data.getncattr('FIRE_NAME')
+        objectID = data.getncattr('OBJECTID')
+        startDate = dt.strptime(data.getncattr('ALARM_DATE'), '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y-%m-%d')
+        features = list()
+
+        try:
+            endDate = dt.strptime(data.getncattr('CONT_DATE'), '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y-%m-%d')
+        except ValueError:
+            endDate = '0000-00-00'
+        ax.set_title("%s - California Wildfire (%s - %s)" % (name, startDate, endDate))
+        ax.coastlines('10m')
+        ax.set_xticks([*range(-115,-125,1)], crs=ccrs.PlateCarree())
+        ax.set_yticks([*range(33,45,1)], crs=ccrs.PlateCarree())
+        ax.pcolormesh(
+                    lon,
+                    lat,
+                    empty,
+                    shading='auto',
+                    alpha=0.3,
+                    cmap=cmap
+                )
+        cs = ax.contourf(
+            lon,
+            lat,
+            empty,
+            alpha=0
+        )
+        for collection in cs.collections:
+            for path in collection.get_paths():
+                # Save geojson -- copied from original code
+                if path.to_polygons():
+                    for npoly, polypoints in enumerate(path.to_polygons()):
+                        # REMINDER: lat and lon positions need to be the reverse of
+                        # how they're passed to the plot
+                        poly_lats = polypoints[:, 0]
+                        poly_lons = polypoints[:, 1]
+                        poly_init = Polygon(
+                            [coords for coords in zip(poly_lats, poly_lons)]
+                        )
+                        if poly_init.is_valid:
+                            poly_clean = poly_init
+                        else:
+                            poly_clean = poly_init.buffer(0.)
+                        if npoly == 0:
+                            poly = poly_clean
+                        else:
+                            poly = poly.difference(poly_clean)
+                    footprint = geojson.Feature(
+                        geometry=poly,
+                        properties={
+                            'anomaly': str(i),
+                            'dateTime': dt.strptime(data.getncattr('CONT_DATE'), '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y%m%d%H%M%S')
+                        }
+                    )
+                    features.append(footprint)
+        gj = geojson.FeatureCollection(features)
+        jobID = maskFile.split('/')[-1].split('-')[0] + '-' + maskFile.split('/')[-1].split('-')[1]
+        geoJsonFile = f'/data/tmp/{jobID}-{endDate}.json'
+        with open(geoJsonFile, 'w') as f:
+            geojson.dump(gj, f)
+
+        circle = Circle((np.median(g_lon), np.median(g_lat)), radius=0.3, edgecolor='red', facecolor='none', linewidth=2)
+        ax.add_patch(circle)
+        plt.xlabel(f'longitude ({0.005} resolution)')
+        plt.ylabel(f'latitude ({0.005} resolution)')
+        
+        try:
+            plotFile = '/data/tmp/%s-%s.png' % (objectID, dt.strptime(data.getncattr('CONT_DATE'), '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y%m%d%H%M%S'))
+        except ValueError:
+            plotFile = '/data/tmp/%s-%s.png' % (objectID, '00000000000000')
+        plt.savefig(plotFile)
+        plt.close()
+        print('Saving %s' % plotFile)
+
+    return
